1704040b5b2615673dcbe98b9830f2b0a5453009
klee4744 <55144083+klee4744@users.noreply.github.com>
2021-06-28 16:16:34 -0400

    Fix syntax error: add comma;

--- drivers/gpu/drm/bridge/sn65dsi83/sn65dsi83_drv.c
+++ drivers/gpu/drm/bridge/sn65dsi83/sn65dsi83_drv.c
@@ -170,7 +170,7 @@ static void sn65dsi83_bridge_mode_set(struct drm_bridge *bridge,
     drm_mode_copy(&sn65dsi83->curr_mode, adj_mode);
 }
 
-static int sn65dsi83_bridge_attach(struct drm_bridge *bridge
+static int sn65dsi83_bridge_attach(struct drm_bridge *bridge,
 				   enum drm_bridge_attach_flags flags)
 {
     struct sn65dsi83 *sn65dsi83 = bridge_to_sn65dsi83(bridge);
